eae45bd15d624b37c64d64b9f5f5d0f52e3cb398
Anthony Ku <anthony.yc.ku@gmail.com>
2020-12-26 01:26:41 -0500

    updated validation

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,18 +4,54 @@ const users = express.Router();
 const User = require("../models/users.js");
 
 //New sign up page
+
+let passwordMatch;
+let userBefore;
+let userExists;
 users.get("/new", (req, res) => {
+    passwordMatch = true;
+    userBefore = req.body.username;
+    userExists = false;
     res.render("users/new.ejs", {
-        currentUser: req.session.currentUser
+        userExists: userExists,
+        user: userBefore,
+        currentUser: req.session.currentUser,
+        match: passwordMatch
     });
 })
 
 users.post("/", (req, res) => {
-    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-    User.create(req.body, (err, createdUser) => {
-        console.log("User is created: " + createdUser);
-        res.redirect("/");
-    })
+
+    if (req.body.password === req.body.confirmPassword) {
+
+
+        passwordMatch = true;
+        req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+        User.create(req.body, (err, createdUser) => {
+            console.log("User is created: " + createdUser);
+            if (createdUser === undefined) {
+                userExists = true;
+                res.render("users/new.ejs", {
+                    userExists: userExists,
+                    user: userBefore,
+                    currentUser: req.session.currentUser,
+                    match: passwordMatch
+                });
+            } else {
+                res.redirect("/");
+            }
+        })
+    } else {
+        userBefore = req.body.username;
+        passwordMatch = false;
+        userExists = false;
+        res.render("users/new.ejs", {
+            userExists: userExists,
+            user: userBefore,
+            currentUser: req.session.currentUser,
+            match: passwordMatch
+        });
+    }
 })
 
 module.exports = users;
