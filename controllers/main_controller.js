1a73ce53aa554cecdbe2959189d3775d8923046e
Anthony Ku <anthony.yc.ku@gmail.com>
2020-12-25 21:44:00 -0500

    added routes

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -35,7 +35,7 @@ router.get('/:page', (req, res) => {
                     pages: Math.ceil(numberOfProfiles / perPage),
                     results: numberOfProfiles
                 })
-            }), 0);
+            }), 100);
         })
         .skip((perPage * page) - (perPage))
         .limit(perPage);
@@ -44,13 +44,13 @@ router.get('/:page', (req, res) => {
 
 
 // NEW
-router.get('/new', (req, res) => {
-    res.render(
-        'main/new.ejs', { currentUser: req.session.currentUser }
-    )
+router.get('/new', isAuthenticated, (req, res) => {
+    res.render('main/new.ejs', {
+        currentUser: req.session.currentUser
+    })
 })
 
-router.post('/', isAuthenticated, (req, res) => {
+router.post('/', (req, res) => {
     Paws.create(req.body, (error, createdPet) => {
         res.redirect('/main')
     })
@@ -93,15 +93,37 @@ router.get('/:id', (req, res) => {
 
 // SEED ROUTE
 router.get('/setup/seed', isAuthenticated, (req, res) => {
-    Paws.create({},
-        (error, data) => {
-            res.redirect('/main')
-        }
-    )
+    Paws.insertMany([{
+        name: "1",
+        age: "1yr 2mo",
+        breed: "golden retriever"
+    }, {
+        name: "2",
+        age: "1yr 5mo",
+        breed: "golden reliever"
+    }, {
+        name: "3",
+        age: "6yr",
+        breed: "Dachsund"
+    }, {
+        name: "4",
+        age: "not born",
+        breed: "frog"
+    }, {
+        name: "5",
+        age: "13yr",
+        breed: "gangster"
+    }, {
+        name: "6",
+        age: "2mo",
+        breed: "big dude"
+    }], (error, data) => {
+        res.redirect('/')
+    })
 })
 
 // NUKE ROUTE
-router.put("/setup/nuke", isAuthenticated, (req, res) => {
+router.delete("/setup/nuke", isAuthenticated, (req, res) => {
     Paws.deleteMany({}, (err, deleted) => {
         res.redirect("/");
     })
